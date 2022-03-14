9c6e774910000c96e112fa0bdf6a9c75727c2693
rakibul-islam-github <freelancing.everyday@gmail.com>
2022-03-14 23:44:10 +0600

    context api added

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from '../components/NavbarComponent';
+import { createContext, useState } from 'react';
 
+export const userContext = createContext();
 
 function MyApp({ Component, pageProps }) {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-  <>
+  
+  <userContext.Provider value={[loggedInUser, setLoggedInUser]}>
   <NavbarComponent />
   <div className='container'> 
   
   <Component {...pageProps} />
   </div>
-  </>
+  </userContext.Provider>
+  
   )
   
   
